556cfca3f470471a87c0e59aff6ca29cd9ba49fc
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-09-14 11:18:51 -0300

    Feat: Configurando o CORS para o consumo no frontend

---
+++ src/main/java/com/example/amigo_de_patas/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.example.amigo_de_patas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200") // front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+
+                registry.addMapping("/**")
+                        .allowedOrigins("https://gabrielnunes5.github.io/Amigo-de-Patas-Front")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
