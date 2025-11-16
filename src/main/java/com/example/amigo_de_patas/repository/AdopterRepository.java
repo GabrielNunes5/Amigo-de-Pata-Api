03bfdf8b3ad863c4f994fa653c1865a9a7c383d8
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-11-16 17:46:58 -0300

    FEAT: adicionando novos metodos para encontrar adopter

--- src/main/java/com/example/amigo_de_patas/repository/AdopterRepository.java
+++ src/main/java/com/example/amigo_de_patas/repository/AdopterRepository.java
@@ -1,9 +1,6 @@
 package com.example.amigo_de_patas.repository;
 
 import com.example.amigo_de_patas.model.Adopter;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
