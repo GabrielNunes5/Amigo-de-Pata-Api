e48d1f28d7063ef27e8e32e9c15afc59f8cd1200
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2025-11-16 17:41:17 -0300

    FEAT: adicionando novos metodos para encontrar adopter

--- src/main/java/com/example/amigo_de_patas/repository/AdopterRepository.java
+++ src/main/java/com/example/amigo_de_patas/repository/AdopterRepository.java
@@ -1,6 +1,9 @@
 package com.example.amigo_de_patas.repository;
 
 import com.example.amigo_de_patas.model.Adopter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,8 @@ import java.util.UUID;
 @Repository
 public interface AdopterRepository extends JpaRepository<Adopter, UUID> {
     Optional<Adopter> findAdopterByAdopterEmail(String adopterEmail);
+
+    Optional<Adopter> findAdopterByAdopterCPF(String adopterCPF);
+
+    Optional<Adopter> findAdopterByAdopterPhone(String adopterPhone);
 }
