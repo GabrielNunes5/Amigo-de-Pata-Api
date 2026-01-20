3ddc8a1122502422aee7bdf1ab0a37748217d94e
GabrielNunes5 <gabrielnunesdev01@gmail.com>
2026-01-19 22:33:06 -0300

    FEAT: incluindo metedo para verificar idade e passando erro correto

--- src/main/java/com/example/amigo_de_patas/service/AdopterService.java
+++ src/main/java/com/example/amigo_de_patas/service/AdopterService.java
@@ -3,6 +3,8 @@ package com.example.amigo_de_patas.service;
 import com.example.amigo_de_patas.dto.request.AdopterCreateRequest;
 import com.example.amigo_de_patas.dto.request.AdopterUpdateRequest;
 import com.example.amigo_de_patas.dto.response.AdopterResponse;
+import com.example.amigo_de_patas.exceptions.BadRequestException;
+import com.example.amigo_de_patas.exceptions.ConflictException;
 import com.example.amigo_de_patas.exceptions.ResourceNotFoundException;
 import com.example.amigo_de_patas.mapper.AdopterMapper;
 import com.example.amigo_de_patas.model.Adopter;
@@ -11,12 +13,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.UUID;
 
 @Service
 public class AdopterService {
     private final AdopterRepository adopterRepository;
     private final AdopterMapper adopterMapper;
+
+    private boolean isAdult(LocalDate birthDate) {
+        return Period.between(birthDate, LocalDate.now()).getYears() >= 18;
+    }
 
     public AdopterService(AdopterRepository adopterRepository, AdopterMapper adopterMapper) {
         this.adopterRepository = adopterRepository;
@@ -25,6 +34,22 @@ public class AdopterService {
 
     @Transactional
     public AdopterResponse createAdopter(AdopterCreateRequest req){
+        if (!isAdult(req.getAdopterBirthDate())) {
+            throw new BadRequestException("O adotante deve ter pelo menos 18 anos");
+        }
+
+        if (adopterRepository.findAdopterByAdopterEmail(req.getAdopterEmail()).isPresent()) {
+            throw new ConflictException("Email já cadastrado");
+        }
+
+        if (adopterRepository.findAdopterByAdopterCPF(req.getAdopterCPF()).isPresent()) {
+            throw new ConflictException("CPF já cadastrado");
+        }
+
+        if (adopterRepository.findAdopterByAdopterPhone(req.getAdopterPhone()).isPresent()){
+            throw new ConflictException("Numero de Telefone ja cadastrado");
+        }
+
         Adopter entity = adopterMapper.toEntity(req);
         Adopter saved = adopterRepository.save(entity);
         return adopterMapper.toResponse(saved);
